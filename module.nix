5e30e68591f6c00460e99e88f8eb74ce7c2d8494
Kai Wohlfahrt <kai@prodo.ai>
2018-10-22 14:15:52 +0100

    Replace unit file with Nix module

---
+++ module.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.services.dyndns;
+  inherit (lib) types mkOption mkIf;
+in {
+  options.services.dyndns = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable the Dyndns dynamic DNS service.
+      '';
+    };
+    interface = mkOption {
+      type = types.string;
+      description = ''
+        Which interface to monitor for IP address changes.
+      '';
+    };
+    url = mkOption {
+      type = types.string;
+      description = ''
+        Which URL to GET in response to changes.
+      '';
+    };
+    class = mkOption {
+      type = types.nullOr (types.enum ["4" "6"]);
+      description = ''
+        Whether to listen only for IPv4 or IPv6 address chantges (default both).
+      '';
+    };
+    allow_private = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to update when a private-range IP address is detected.
+      '';
+    };
+  };
+
+  config.systemd.services.dyndns = mkIf cfg.enable {
+    description = "Small Dynamic DNS Client";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "notify";
+      ExecStart = let
+        flags = lib.concatStringsSep " "
+          (["-${cfg.class or "46"}"] ++ lib.optional cfg.allow_private "--allow-private");
+        in "${pkgs.dyndns}/bin/dyndns ${flags} ${cfg.interface} ${cfg.url}";
+    };
+  };
+}
